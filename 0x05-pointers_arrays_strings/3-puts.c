35b4719f3b2a4ff733e0730ae20b09ef84c4aa54
Nyamisam <davinemachogu@gmail.com>
2023-02-27 10:56:25 -0800

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -3,12 +3,15 @@
 /**
  * _puts - prints a string
  * @str: string to be printed
+ * Return: 0
  */
 void _puts(char *str)
 {
-while (*str != '\0')
+int i = 0;
+while (str[i])
 {
-_putchar(*str++);
+_putchar(str[i]);
+i++
 }
 _putchar('\n');
 }
